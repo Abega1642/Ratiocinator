dd41e04cc0f372149002eef893a3336397bf7982
Abega1642 <hei.abega.2@gmail.com>
2024-08-27 10:02:35 +0300

    feat: add some test in src/test/

---
+++ src/test/java/TestForRatiocinator.java
@@ -0,0 +1,60 @@
+import dev.razafindratelo.checker.FalseTautology;
+import dev.razafindratelo.checker.Statement;
+import dev.razafindratelo.checker.TrueTautology;
+import dev.razafindratelo.checker.TruthValue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestForRatiocinator {
+    @Test
+    void lou_is_handsome() {
+        var lou_is_handsome = new TrueTautology("Lou is handsome");
+        Assertions.assertEquals(TruthValue.TRUE, lou_is_handsome.apply());
+    }
+
+    @Test
+    void lou_is_poor() {
+        var lou_is_poor = new FalseTautology("Lou is poor");
+        Assertions.assertEquals(TruthValue.FALSE, lou_is_poor.apply());
+    }
+
+    @Test
+    void lou_is_generous() {
+        var lou_is_generous = new Statement("Lou is generous");
+        Assertions.assertEquals(TruthValue.UNKNOWN, lou_is_generous.apply());
+    }
+
+    @Test
+    void lou_is_poor_AND_lou_is_generous() {
+        var lou_is_poor = new FalseTautology("Lou is poor");
+        var lou_is_generous = new Statement("Lou is generous");
+
+        Assertions.assertEquals(TruthValue.UNKNOWN, lou_is_poor.and(lou_is_generous).apply());
+    }
+
+    @Test
+    void lou_is_handsome_SO_lou_is_poor() {
+        var lou_is_handsome = new TrueTautology("Lou is handsome");
+        var lou_is_poor = new FalseTautology("Lou is poor");
+
+        Assertions.assertEquals(TruthValue.FALSE, lou_is_handsome.then(lou_is_poor).apply());
+    }
+
+    @Test
+    void lou_is_poor_SO_lou_is_generous() {
+        var lou_is_poor = new FalseTautology("Lou is poor");
+        var lou_is_generous = new Statement("Lou is generous");
+
+        Assertions.assertEquals(TruthValue.UNKNOWN, lou_is_poor.then(lou_is_generous).apply());
+    }
+
+    @Test
+    void lou_is_handsome_OR_lou_is_generous_SO_lou_is_poor() {
+        var lou_is_handsome = new TrueTautology("Lou is handsome");
+        var lou_is_generous = new Statement("Lou is generous");
+        var lou_is_poor = new FalseTautology("Lou is poor");
+
+
+        Assertions.assertEquals(TruthValue.UNKNOWN, lou_is_handsome.or(lou_is_generous).apply());
+    }
+}
