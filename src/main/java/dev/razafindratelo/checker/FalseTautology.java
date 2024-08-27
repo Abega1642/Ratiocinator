75b9825a878ee0475b0319c3f229464bae5d4814
Abega1642 <hei.abega.2@gmail.com>
2024-08-27 10:01:37 +0300

    feat: add FalseTautology class

---
+++ src/main/java/dev/razafindratelo/checker/FalseTautology.java
@@ -0,0 +1,13 @@
+package dev.razafindratelo.checker;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class FalseTautology extends Statement{
+
+    public FalseTautology(String statement) {
+        super(statement, TruthValue.FALSE);
+    }
+}
